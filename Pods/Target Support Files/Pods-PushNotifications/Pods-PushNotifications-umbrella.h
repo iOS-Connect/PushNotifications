9d67b965a95ec0f05e78d61dcd91ef88d62bd28f
yhu98 <yhu98@ucsc.edu>
2016-12-11 14:06:21 -0800

    pod installed

---
+++ Pods/Target Support Files/Pods-PushNotifications/Pods-PushNotifications-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PushNotificationsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PushNotificationsVersionString[];
+
