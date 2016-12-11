bb3d3fe760d8cbf1f45d7b59a508be8733265575
John Regner <regnerjr@gmail.com>
2016-12-11 15:55:43 -0800

    First Extraction of Push Manager

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -10,33 +10,48 @@ import UIKit
 import UserNotifications
 import PusherSwift
 
-let pusherKey = "5637213a207393e799e6"
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let pusher = Pusher(key: pusherKey)
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 10.0, *) {
-            pusher.connect()
-            let center = UNUserNotificationCenter.current()
-            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-                // actions based on whether notifications were authorized or not
-            }
-        } else {
-            // Fallback on earlier versions
-        }
+    let pushManager = PushManager()
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        pushManager.firstLaunch()
+        pushManager.askPermissions()
         application.registerForRemoteNotifications()
         return true
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        pushManager.registrationSuccessFull(token: deviceToken)
+    }
+
+}
+
+class PushManager {
+    static let pusherKey = "5637213a207393e799e6"
+    let pusher = Pusher(key: PushManager.pusherKey)
+
+    func firstLaunch() {
+        pusher.connect()
+    }
+
+    func askPermissions() {
+        if #available(iOS 10.0, *) {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                // actions based on whether notifications were authorized or not
+            }
+        } else {
+
+        }
+
+    }
+
+    func registrationSuccessFull(token: Data) {
         pusher.nativePusher().register(deviceToken: deviceToken)
         pusher.nativePusher().subscribe(interestName: "donuts")
     }
-
 }
 
